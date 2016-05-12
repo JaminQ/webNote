8df3e9467d77a1abe739c0659ca386a47b1f8199
JaminQian <jaminqian@outlook.com>
2016-05-12 17:39:26 +0800

    change html construction

--- js/module/content.html.js
+++ js/module/content.html.js
@@ -1,5 +1,5 @@
-define(function() {
-	return {
-		"data": "<div id='fly2-wrapper'>\n<a class='search-storage'><i class='fa fa-book'></i></a>\n<a class='search-call'><i class='fa'>旺</i></a>\n<a class='search-stream'><i class='fa fa-truck'></i></a>\n<a class='search-details'><i class='fa fa-info'></i></a>\n</div>\n<div id='fly2-storage'>\n<div class='fly2-header'>\n<div class='fly2-header-title'>让小二飞 - 我的知识库</div>\n<div class='fly2-storage-search'>\n<i class='fa fa-search'></i>\n<input type='text' placeHolder='输入你要检索的知识库关键字' />\n</div>\n</div><div class='main-content'></div>\n<i class='closeFly2 fa fa-times'></i>\n</div>"
-	};
+define(function(){
+    return {
+        "data":"<div id='webNote-wrapper'>\n<a class='search-storage'>\n<i class='fa fa-book'></i>\n</a>\n</div>\n<div id='webNote-storage'>\n<div class='webNote-header'>\n<div class='webNote-header-title'>个人笔记</div>\n<div class='webNote-storage-search'>\n<i class='fa fa-search'></i>\n<input type='text' placeHolder='输入你要检索的知识库关键字' />\n</div>\n<i class='closeWebNote fa fa-times'></i>\n</div>\n<div class='main-content'></div>\n</div>"
+    };
 });
