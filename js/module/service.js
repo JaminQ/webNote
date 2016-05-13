126c9301b185583cd4fbd874520c17e40d851799
JaminQian <jaminqian@outlook.com>
2016-05-13 20:05:31 +0800

    update url

---
+++ js/module/service.js
@@ -0,0 +1,9 @@
+/*global define, chrome*/
+
+define(function() {
+    'use strict';
+    var Service = function() {
+        this.URL = '//localhost:80/webNote/index.php';
+    };
+    return Service;
+});
