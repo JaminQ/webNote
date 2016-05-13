24eec4b517cf10f1fefba4e6e60ff1050ffc4998
JaminQian <jaminqian@outlook.com>
2016-05-13 20:05:21 +0800

    add jquery dependence

--- js/lib/serializeObject.js
+++ js/lib/serializeObject.js
@@ -1,23 +1,26 @@
-$.fn.extend({
-	/**
-	 返回Object对象，key是每个表单元素的name，value是值，相同name元素的值会被覆盖，最后的有效<br/>
-	 基于serializeArray转换：http://www.w3school.com.cn/jquery/ajax_serializearray.asp
+require(['jquery'], function($) {
+    $.fn.extend({
+        /**
+         返回Object对象，key是每个表单元素的name，value是值，相同name元素的值会被覆盖，最后的有效<br/>
+         基于serializeArray转换：http://www.w3school.com.cn/jquery/ajax_serializearray.asp
 
-	 @class serializeObject
-	 @namespace $.fn
-	 @example
-	 	req("common/serializeObject");
-	  	$('form').serializeObject();
-	  	$('input').serializeObject();
+         @class serializeObject
+         @namespace $.fn
+         @example
+         	req("common/serializeObject");
+          	$('form').serializeObject();
+          	$('input').serializeObject();
 
-	 */
-    serializeObject : function(){
+         */
+        serializeObject: function() {
 
-    	var arr = this.serializeArray(), obj = {};
-    	$(arr).each(function(i, item) {
-    		obj[item.name] = typeof item.value === "string" ? item.value.replace(/\u00A0/ig, " ") : item.value;
-    	});
+            var arr = this.serializeArray(),
+                obj = {};
+            $(arr).each(function(i, item) {
+                obj[item.name] = typeof item.value === "string" ? item.value.replace(/\u00A0/ig, " ") : item.value;
+            });
 
-    	return obj;
-    }
+            return obj;
+        }
+    });
 });
