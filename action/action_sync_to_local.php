fd8dd652333ebff989afca1d2d4b2ea1d9436b28
JaminQian <jaminqian@outlook.com>
2016-05-18 02:01:04 +0800

    update message

--- action/action_sync_to_local.php
+++ action/action_sync_to_local.php
@@ -12,11 +12,11 @@ $result = $this->db->select($table, $columnName, $condition);
 
 if($result->num_rows > 0) {
     $obj = $result->fetch_object();
-    $res = array('data' => array('data' => $obj->data), 'status' => 1, 'msg' => '获取云端数据成功');
-    $this->console->log('获取云端数据成功');
+    $res = array('data' => array('data' => $obj->data), 'status' => 1, 'msg' => '云端数据同步到本地成功');
+    $this->console->log('云端数据同步到本地成功');
 } else {
-    $res = array('data' => '', 'status' => 0, 'msg' => '获取云端数据失败');
-    $this->console->log('获取云端数据失败');
+    $res = array('data' => '', 'status' => 0, 'msg' => '云端数据同步到本地失败');
+    $this->console->log('云端数据同步到本地失败');
 }
 
 $result->free();
