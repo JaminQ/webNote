0202dc5d7149e8e94511dc4e653d3be36598c143
JaminQian <jaminqian@outlook.com>
2016-05-20 01:22:45 +0800

    update config

--- config.php
+++ config.php
@@ -1,28 +1,16 @@
 <?php if (!defined('IF_AUTHORIZED')) { die('Unauthorized Access!'); }
 
 // Some database configuration
-define('MYSQL_USER', 'root');
-define('MYSQL_PASSWORD', '11');
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_PORT', '3306');
-define('MYSQL_DB', 'test');
+define('MYSQL_USER', '<your_mysql_user>');
+define('MYSQL_PASSWORD', '<your_mysql_password>');
+define('MYSQL_HOST', '<your_mysql_host>');
+define('MYSQL_PORT', '<your_mysql_port>');
+define('MYSQL_DATABASE', '<your_mysql_database>');
+define('MYSQL_CODING', '<your_mysql_charset>');
 
 // Log path
 define('LOG_PATH', __DIR__ . '/_log/');
-
-// Log function
-function mylog($str) {
-    if (!is_string($str)) {
-        $str = json_encode($str);
-    }
-    $file = LOG_PATH . 'log.txt';
-    // if (file_exists($file)) {
-    //     file_put_contents($file, '');
-    // }
-    $fp = fopen($file, 'a');
-    fwrite($fp, date('[m-d H:i:s]')." ".$str."\n");
-    fclose($fp);
-}
+define('LOG_FILE', 'log.txt');
 
 /* END file */
 ?>
